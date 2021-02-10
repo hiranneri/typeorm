76dff6de880deb2bf4c479569b12a9fee1993378
Hiran <hiran.souza93@gmail.com>
2021-02-09 22:19:03 -0300

    trying to fix heroku configs

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,7 @@ console.log("process.env.DATABASE_URL : ",process.env.DATABASE_URL)
 module.exports =  {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "port": process.env.PORT || 5000,
   "entities": [
     "dist/models/**/*.js"
  ],
